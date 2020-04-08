efb9e237923b3d30d78be7d2aab0a51945796adc
haneul <zr420cha@gmail.com>
2020-04-08 20:34:01 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>This is the App</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
